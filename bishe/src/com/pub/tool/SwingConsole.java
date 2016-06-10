cf2ef76b6dec4e6672cf406259b4764d93523ce4
songkaojun <songkaojun@010D1407230032.58ganji-corp.com>
2016-06-10 13:50:36 +0800

    songkaojun

--- bishe/src/com/pub/tool/SwingConsole.java
+++ bishe/src/com/pub/tool/SwingConsole.java
@@ -3,7 +3,7 @@ package com.pub.tool;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 /**
-*哈哈哈哈哈哈哈
+*哈哈哈哈哈哈哈songkaojun
 */
 public class SwingConsole {
 	public static void run(final JFrame f,final int width,final int height) {
